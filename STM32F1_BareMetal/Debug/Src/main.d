8af958c5d24c5923017120458da8ec830a60d79f
ArmandJordaan97 <armandjordaan97@gmail.com>
2024-01-24 18:03:46 +0200

    Initial Commit

---
+++ STM32F1_BareMetal/Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/Armand/Documents/STM32\ Programs/STM32F1_BareMetal_Template/STM32F1_BareMetal/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ C:/Users/Armand/Documents/STM32\ Programs/STM32F1_BareMetal_Template/STM32F1_BareMetal/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ C:/Users/Armand/Documents/STM32\ Programs/STM32F1_BareMetal_Template/STM32F1_BareMetal/Drivers/CMSIS/Core/Include/core_cm3.h \
+ C:/Users/Armand/Documents/STM32\ Programs/STM32F1_BareMetal_Template/STM32F1_BareMetal/Drivers/CMSIS/Core/Include/cmsis_version.h \
+ C:/Users/Armand/Documents/STM32\ Programs/STM32F1_BareMetal_Template/STM32F1_BareMetal/Drivers/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Users/Armand/Documents/STM32\ Programs/STM32F1_BareMetal_Template/STM32F1_BareMetal/Drivers/CMSIS/Core/Include/cmsis_gcc.h \
+ C:/Users/Armand/Documents/STM32\ Programs/STM32F1_BareMetal_Template/STM32F1_BareMetal/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+C:/Users/Armand/Documents/STM32\ Programs/STM32F1_BareMetal_Template/STM32F1_BareMetal/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+C:/Users/Armand/Documents/STM32\ Programs/STM32F1_BareMetal_Template/STM32F1_BareMetal/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+C:/Users/Armand/Documents/STM32\ Programs/STM32F1_BareMetal_Template/STM32F1_BareMetal/Drivers/CMSIS/Core/Include/core_cm3.h:
+C:/Users/Armand/Documents/STM32\ Programs/STM32F1_BareMetal_Template/STM32F1_BareMetal/Drivers/CMSIS/Core/Include/cmsis_version.h:
+C:/Users/Armand/Documents/STM32\ Programs/STM32F1_BareMetal_Template/STM32F1_BareMetal/Drivers/CMSIS/Core/Include/cmsis_compiler.h:
+C:/Users/Armand/Documents/STM32\ Programs/STM32F1_BareMetal_Template/STM32F1_BareMetal/Drivers/CMSIS/Core/Include/cmsis_gcc.h:
+C:/Users/Armand/Documents/STM32\ Programs/STM32F1_BareMetal_Template/STM32F1_BareMetal/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
